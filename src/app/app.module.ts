f609659eaa837db4d4cd1fbabc599b5fd81f5995
Ranu <ranu.miah@outlook.com>
2017-07-27 18:05:23 +0100

    added basic compoents - onto gallery pictures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavmenuComponent,
+    GalleryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'gallery', component: GalleryComponent },
+      { path: '**', redirectTo: 'home' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
